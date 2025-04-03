b6b31db9c2df29f3e0e42ec50f4dca83049c59de
Juun <blacklily7513@gmail.com>
2025-04-03 12:47:07 +0900

    chore: Update entry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import Collection from './collection.js';
-import TypeGuard from './utils/type-guard.js';
-
-export { Collection, TypeGuard };
-export default Collection;
+export { default as Collection } from './collection.js';
+export { HybridTerrainProvider } from './terrain/hybrid-terrain-provider.js';
+export { TerrainArea } from './terrain/terrain-area.js';
+export { TerrainBounds } from './terrain/terrain-bounds.js';
+export { TerrainVisualizer } from './utils/terrain-visualizer.js';
+export { default as TypeGuard } from './utils/type-guard.js';
