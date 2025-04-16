43e2f1e7ad9267a212e9cbc1e05f1c5206a5452f
Juun <blacklily7513@gmail.com>
2025-04-16 16:08:41 +0900

    test: Restructure require and import tests
    * Use vitest to test.
    
    * Move tests directory under src in order to be covered by `tsconfig.json`.

---
+++ src/__tests__/require-import/cjs-require.test.cjs
@@ -0,0 +1,22 @@
+const { Viewer } = require('cesium');
+const utils = require('../../../dist/index.cjs');
+const { test } = require('vitest/dist/index.js');
+const { expect } = require('vitest/dist/index.js');
+const { describe } = require('vitest/dist/index.js');
+
+describe('Common JS require test with Cesium', () => {
+  test('require from Cesium', () => {
+    expect(Viewer).toBeDefined();
+  });
+
+  test('require from dist', () => {
+    expect(utils.cloneViewer).toBeDefined();
+    expect(utils.Collection).toBeDefined();
+    expect(utils.HybridTerrainProvider).toBeDefined();
+    expect(utils.TerrainArea).toBeDefined();
+    expect(utils.TerrainAreas).toBeDefined();
+    expect(utils.TerrainBounds).toBeDefined();
+    expect(utils.TerrainVisualizer).toBeDefined();
+    expect(utils.syncCamera).toBeDefined();
+  })
+})
---
+++ src/__tests__/require-import/esm-import.test.mjs
@@ -0,0 +1,20 @@
+import { Viewer } from 'cesium';
+import * as utils from '../../../dist/index.js';
+import { describe, expect, test } from 'vitest';
+
+describe('ECMA Script Module import test with Cesium', () => {
+  test('import from Cesium', () => {
+    expect(Viewer).toBeDefined();
+  });
+
+  test('import from dist', () => {
+    expect(utils.cloneViewer).toBeDefined();
+    expect(utils.Collection).toBeDefined();
+    expect(utils.HybridTerrainProvider).toBeDefined();
+    expect(utils.TerrainArea).toBeDefined();
+    expect(utils.TerrainAreas).toBeDefined();
+    expect(utils.TerrainBounds).toBeDefined();
+    expect(utils.TerrainVisualizer).toBeDefined();
+    expect(utils.syncCamera).toBeDefined();
+  });
+});
