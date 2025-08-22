56755ff8f9e3775d7245e69f3636e925d3b1fc30
Juun <blacklily7513@gmail.com>
2025-08-22 13:57:14 +0900

    fix: Update build and document configuration

--- src/__tests__/require-import/cjs-require.test.cjs
+++ src/__tests__/require-import/cjs-require.test.cjs
@@ -1,18 +1,17 @@
-const { Viewer } = require('cesium');
+const Cesium = require('cesium');
 const { describe, expect, test } = require('vitest/dist/index.js');
 const utils = require('../../../dist/index.cjs');
+const dev = require('../../../dist/dev/index.cjs');
+const exp = require('../../../dist/experimental/index.cjs');
 
 describe('Common JS require test with Cesium', () => {
   test('require from Cesium', () => {
-    expect(Viewer).toBeDefined();
+    expect(Cesium).toBeDefined();
   });
 
   test('require from dist', () => {
-    expect(utils.Collection).toBeDefined();
-    expect(utils.Highlight).toBeDefined();
-    expect(utils.HybridTerrainProvider).toBeDefined();
-
-    expect(utils.cloneViewer).toBeDefined();
-    expect(utils.syncCamera).toBeDefined();
+    expect(utils).toBeDefined();
+    expect(dev).toBeDefined();
+    expect(exp).toBeDefined();
   })
 })
--- src/__tests__/require-import/esm-import.test.mjs
+++ src/__tests__/require-import/esm-import.test.mjs
@@ -1,18 +1,17 @@
-import { Viewer } from 'cesium';
+import * as Cesium from 'cesium';
 import * as utils from '../../../dist/index.js';
+import * as dev from "../../../dist/dev/index.js"
+import * as exp from "../../../dist/experimental/index.js"
 import { describe, expect, test } from 'vitest';
 
 describe('ECMA Script Module import test with Cesium', () => {
   test('import from Cesium', () => {
-    expect(Viewer).toBeDefined();
+    expect(Cesium).toBeDefined();
   });
 
   test('import from dist', () => {
-    expect(utils.Collection).toBeDefined();
-    expect(utils.Highlight).toBeDefined();
-    expect(utils.HybridTerrainProvider).toBeDefined();
-
-    expect(utils.cloneViewer).toBeDefined();
-    expect(utils.syncCamera).toBeDefined();
+    expect(utils).toBeDefined();
+    expect(dev).toBeDefined();
+    expect(exp).toBeDefined();
   });
 });
